66c0709787520dd99d326bab8793747a69bfc950
Sonu Sekhon <ssekhon@xmatters.com>
2018-01-17 11:57:41 -0800

    Demo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var multer  = require('multer')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -16,6 +17,25 @@ app.get('/', function(request, response) {
 
 app.get('/cool', function(request, response) {
   response.send(cool());
+});
+
+var upload = multer({ dest: './uploads' });
+
+app.post('/parse', function (req, res) {
+  var from = req.body.from;
+  var text = req.body.text;
+  var subject = req.body.subject;
+  var num_attachments = req.body.attachments;
+  for (i = 1; i <= num_attachments; i++){
+    var attachment = req.files['attachment' + i];
+    // attachment will be a File object
+  }
+
+  var oNode = text.evaluate('//a[1]', // XPath for first link in the document
+        document, null, 0, 0
+        ).iterateNext();
+  console.log('[' + oNode.text + '](' + oNode.href + ')');
+
 });
 
 app.listen(app.get('port'), function() {
